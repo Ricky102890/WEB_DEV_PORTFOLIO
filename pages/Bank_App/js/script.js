fc7567df4653ea6fe01dd7505d6cd1e04064befa
CJAX.Dev <jackson102890@gmail.com>
2025-07-17 14:39:06 -0600

    Add Hamburger Icon and script connection

--- pages/Bank_App/js/script.js
+++ pages/Bank_App/js/script.js
@@ -1,28 +1,47 @@
 // Checkings Account 
 // Variables and DOM
-let checkingsAccountNum;
-let checkingsAge;
-let checkingsAccountNumOutput = document.getElementById("c-acct-num");
-let checkingAccountAgeOutput = document.getElementById("c-acct-age");
+// let checkingsAccountNum;
+// let checkingsAge;
+// let checkingsAccountNumOutput = document.getElementById("c-acct-num");
+// let checkingAccountAgeOutput = document.getElementById("c-acct-age");
 
 // Checkings Balance and DOM
-let checkingsBalance;
-checkingsBalance = 2921.32;
-let checkingsBalanceSpan = document.querySelector(".checkings-balance-box");
-checkingsBalanceSpan.style.color = "#ffd900"
-checkingsBalanceSpan.innerHTML = `Current Balance: $ ${checkingsBalance}`;
+// let checkingsBalance;
+// checkingsBalance = 2921.32;
+// let checkingsBalanceSpan = document.querySelector(".checkings-balance-box");
+// checkingsBalanceSpan.style.color = "#ffd900"
+// checkingsBalanceSpan.innerHTML = `Current Balance: $ ${checkingsBalance}`;
 
-checkingsAccountNum = 2957448395;
-checkingsAge = `433 Days`;
-checkingAccountAgeOutput.innerHTML = checkingsAge;
-checkingsAccountNumOutput.innerHTML = checkingsAccountNum;
+// checkingsAccountNum = 2957448395;
+// checkingsAge = `433 Days`;
+// checkingAccountAgeOutput.innerHTML = checkingsAge;
+// checkingsAccountNumOutput.innerHTML = checkingsAccountNum;
 
 // Checkings Account Styles
-checkingAccountAgeOutput.style.color = "#ffd900";
-checkingsAccountNumOutput.style.color = "#ffd900";
+// checkingAccountAgeOutput.style.color = "#ffd900";
+// checkingsAccountNumOutput.style.color = "#ffd900";
 
 // Checkings Account Recent Transactions Sample
-const recentTransBox = document.querySelector(".c-trans-content");
+// const recentTransBox = document.querySelector(".c-trans-content");
 
 
 // Checkings Account Recent Transactions Format
+
+
+// Navigation Menu / Sub Menu
+function showNav() {
+    const navbar = document.getElementById('navbar');
+    const close_icon = document.getElementById('close_nav_icon');
+    const ham_btn = document.getElementById('ham_btn');
+    ham_btn.addEventListener('click', () => {
+        if (ham_btn.getAttribute = 'false') {
+            ham_btn.setAttribute('aria-expanded', 'true');
+            ham_btn.classList.add('rotate_trans');
+        }
+    })
+    close_icon.addEventListener('click', () => {
+        ham_btn.setAttribute('aria-expanded', 'false');
+        ham_btn.classList.remove('rotate_trans');
+    })
+}
+showNav();
